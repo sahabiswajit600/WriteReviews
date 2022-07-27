764c8814dbf6bd6863fe0f0f799847f2c7e0b8bf
Biswajit Saha <sahabiswajit600@gmail.com>
2022-07-27 16:07:01 +0530

    error handling

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
+const catchAsync = require('./utils/catchAsync');
 const methodOverride = require('method-override');
 const Review = require('./models/review');
 
@@ -18,53 +19,57 @@ db.once("open", () => {
 
 const app = express();
 
-app.engine('ejs', ejsMate)
+app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'))
+app.set('views', path.join(__dirname, 'views'));
 
-app.use(express.urlencoded({ extended: true}))
+app.use(express.urlencoded({ extended: true}));
 app.use(methodOverride('_method'));
 
 app.get('/', (req, res) => {
     res.render('home')
 });
-app.get('/reviews', async (req, res) => {
+app.get('/reviews', catchAsync(async (req, res) => {
     const reviews = await Review.find({});
     res.render('reviews/index', { reviews })
-});
+}));
 
 app.get('/reviews/new', (req, res) => {
     res.render('reviews/new');
 });
 
-app.post('/reviews', async (req, res) => {
+app.post('/reviews', catchAsync(async (req, res) => {
     const review = new Review(req.body.review);
     await review.save();
     res.redirect(`/reviews/${review._id}`)
-})
+}));
 
-app.get('/reviews/:id', async (req, res) => {
+app.get('/reviews/:id', catchAsync(async (req, res) => {
     const review = await Review.findById(req.params.id)
     res.render('reviews/show', { review });
-});
+}));
 
-app.get('/reviews/:id/edit', async (req, res) => {
+app.get('/reviews/:id/edit', catchAsync(async (req, res) => {
     const review = await Review.findById(req.params.id)
     res.render('reviews/edit', { review });
-});
+}));
 
-app.put('/reviews/:id', async (req, res) => {
+app.put('/reviews/:id', catchAsync(async (req, res) => {
     const { id } = req.params;
     const review = await Review.findByIdAndUpdate(id, {...req.body.review})
     res.redirect(`/reviews/${review._id}`)
-});
+}));
 
-app.delete('/reviews/:id', async (req, res) => {
+app.delete('/reviews/:id', catchAsync(async (req, res) => {
     const { id } = req.params;
     await Review.findByIdAndDelete(id);
     res.redirect('/reviews');
-})
+}));
+
+app.use((err, req, res, next) => {
+    res.send('something went wrong!')
+});
 
 app.listen(3000, () => {
     console.log('Serving on port 3000')
-})
+});
