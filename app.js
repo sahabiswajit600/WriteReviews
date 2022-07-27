25fe4efe3142a3fbcec446766e637ca5fa1a6c62
Biswajit Saha <sahabiswajit600@gmail.com>
2022-07-27 19:54:18 +0530

    error template

--- app.js
+++ app.js
@@ -73,9 +73,9 @@ app.all('*', (req, res, next) => {
 });
 
 app.use((err, req, res, next) => {
-    const {statusCode = 500, message = 'Something went wrong!'} = err;
-    res.status(statusCode).send(message);
-    res.send('something went wrong!');
+    const {statusCode = 500} = err;
+    if(!err.message) err.message = 'Something Went Wrong!';
+    res.status(statusCode).render('error', { err });
 });
 
 app.listen(3000, () => {
