fac77cdb1bcc2059ae4cb5d49247fdecf76890c0
Biswajit Saha <sahabiswajit600@gmail.com>
2022-08-02 11:54:44 +0530

    adding comment model

--- models/review.js
+++ models/review.js
@@ -5,7 +5,13 @@ const ReviewSchema = new Schema({
     title: String,
     image: String,
     description: String,
-    location: String
+    location: String,
+    comments: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Comment'
+        }
+    ]
 });
 
 module.exports = mongoose.model('Review', ReviewSchema);
