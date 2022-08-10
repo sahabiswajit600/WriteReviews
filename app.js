4d9d24ff95855371798e89c61bb8f2b42bdbe573
Biswajit Saha <sahabiswajit600@gmail.com>
2022-08-10 18:52:37 +0530

    configuring passport

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const session = require('express-session');
 const flash = require('connect-flash');
 const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const User = require('./models/user');
 
 const reviews = require('./routes/reviews');
 const comments = require('./routes/comments');
@@ -41,13 +44,27 @@ const sessionConfig = {
         maxAge: 1000*60*60*24*7
     }
 };
+
 app.use(session(sessionConfig));
 app.use(flash());
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.use((req, res, next) => {
     res.locals.success = req.flash('success');
     res.locals.error = req.flash('error');
     next();
+});
+
+app.get('/fakeUser', async (req, res) => {
+    const user = new User({email: 'biswajit@gmail.com', username: 'Biswajit'});
+    const newUser = await User.register(user, 'monkey');
+    res.send(newUser);
 });
 
 app.use('/reviews', reviews);
