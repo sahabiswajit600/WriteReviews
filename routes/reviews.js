40e954dff1f41e161080a2740f7d90552531f4e9
Biswajit Saha <sahabiswajit600@gmail.com>
2022-08-14 12:08:16 +0530

    restructure routes

--- routes/reviews.js
+++ routes/reviews.js
@@ -6,18 +6,17 @@ const { isLoggedIn, isAuthor, validateReview } = require('../middleware');
 
 const Review = require('../models/review');
 
-router.get('/', catchAsync(reviews.index));
+router.route('/')
+    .get(catchAsync(reviews.index))
+    .post(isLoggedIn, validateReview, catchAsync(reviews.createReview));
 
 router.get('/new', isLoggedIn, reviews.renderNewForm);
 
-router.post('/', isLoggedIn, validateReview, catchAsync(reviews.createReview));
-
-router.get('/:id', catchAsync(reviews.showReview));
+router.route('/:id')
+    .get(catchAsync(reviews.showReview))
+    .put(isLoggedIn, isAuthor, validateReview, catchAsync(reviews.updateReview))
+    .delete(isLoggedIn, isAuthor, catchAsync(reviews.deleteReview));
 
 router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(reviews.renderEditForm));
-
-router.put('/:id', isLoggedIn, isAuthor, validateReview, catchAsync(reviews.updateReview));
-
-router.delete('/:id', isLoggedIn, isAuthor, catchAsync(reviews.deleteReview));
 
 module.exports = router;
