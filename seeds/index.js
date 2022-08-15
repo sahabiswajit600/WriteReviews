a13de52c9799b0a7f81a38e067bee3ebbbf801cb
Biswajit Saha <sahabiswajit600@gmail.com>
2022-08-15 11:15:10 +0530

    fixing seeds

--- seeds/index.js
+++ seeds/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const cities = require('./cities');
-const {places, descriptors} = require('./seedHelpers');
+const { places, descriptors } = require('./seedHelpers');
 const Review = require('../models/review');
 
 mongoose.connect('mongodb://localhost:27017/write-review', {
@@ -19,13 +19,22 @@ const sample = array => array[Math.floor(Math.random() * array.length)]
 
 const seedDB = async () => {
     await Review.deleteMany({});
-    for(let i=0; i<50; i++) {
+    for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
         const review = new Review({
             author: '62f5c49033c4dabb1b70b5aa',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
-            image: 'https://source.unsplash.com/collection/483251',
+            images: [
+                {
+                    url: 'https://res.cloudinary.com/sahabiswajit600/image/upload/v1660535215/WriteReviews/wxpzhgibovjb47d5socx.jpg',
+                    filename: 'WriteReviews/wxpzhgibovjb47d5socx'
+                },
+                {
+                    url: 'https://res.cloudinary.com/sahabiswajit600/image/upload/v1660540550/WriteReviews/hdqead9zru10cs5jnj8l.jpg',
+                    filename: 'WriteReviews/hdqead9zru10cs5jnj8l'
+                }
+            ],
             description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quidem cumque odit reiciendis quo fuga quis architecto modi temporibus accusamus. Neque quaerat dignissimos perspiciatis. Unde est dignissimos esse autem maxime sed.'
         })
         await review.save();
