2ada4177b3419e0e40392ddd3290025dfcd42677
Biswajit Saha <sahabiswajit600@gmail.com>
2022-08-19 13:48:29 +0530

    reseeding data

--- seeds/index.js
+++ seeds/index.js
@@ -27,7 +27,10 @@ const seedDB = async () => {
             title: `${sample(descriptors)} ${sample(places)}`,
             geometry: { 
                 type: "Point",
-                coordinates: [-113.133115, 47.020078] 
+                coordinates: [
+                    cities[random1000].longitude,
+                    cities[random1000].latitude
+            ] 
             },
             images: [
                 {
