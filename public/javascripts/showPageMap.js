fc3e03ed9eed328aa55b84b29654247d3fe16306
Biswajit Saha <sahabiswajit600@gmail.com>
2022-08-19 09:54:55 +0530

    centering map on review

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -2,10 +2,11 @@ mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
     container: 'map', // container ID
     style: 'mapbox://styles/mapbox/streets-v11', // style URL
-    center: [-74.5, 40], // starting position [lng, lat]
-    zoom: 4, // starting zoom
+    center: reviewGeometry.coordinates, // starting position [lng, lat]
+    zoom: 8, // starting zoom
     projection: 'globe' // display the map as a 3D globe
 });
-map.on('style.load', () => {
-    map.setFog({}); // Set the default atmosphere style
-});
+
+new mapboxgl.Marker()
+    .setLngLat(reviewGeometry.coordinates)
+    .addTo(map);
